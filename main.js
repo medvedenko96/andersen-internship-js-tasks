037328348e854cb767d29f3a080b99a16bb10a1c
Medvedenko Yaroslav <medvedenko96@gmail.com>
2018-06-18 12:49:27 +0300

    task №1 Singleton id done

---
+++ main.js
@@ -0,0 +1,25 @@
+function Universe() {
+    if (typeof Universe.instance === 'object') {
+        return Universe.instance;
+    }
+    this.size = 100;
+    this.getSize = function () {
+        return this.size
+    };
+    this.setSize = function (size) {
+       this.size = size;
+    };
+    Universe.instance = this;
+    return this
+
+}
+
+
+let a = new Universe();
+let b = new Universe();
+
+console.log(a === b); // должны получить true
+
+console.log(a.getSize(), b.getSize()); // 100, 100
+a.setSize(200);
+console.log(a.getSize(), b.getSize()); // 200, 200
