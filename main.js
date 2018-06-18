7a677399a1b1ed9eda9c6b58ed789e02c421dc9a
Medvedenko Yaroslav <medvedenko96@gmail.com>
2018-06-18 13:48:39 +0300

    task №1 Singleton id done

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 //    - метод getSize - возвращает свойство size.
 //    - метод setSize(size) - установка свойства size.
 //=================================================
-function Universe() {
+/*function Universe() {
     if (typeof Universe.instance === 'object') {
         return Universe.instance;
     }
@@ -20,7 +20,76 @@ function Universe() {
     Universe.instance = this;
     return this
 
-}
+}*/
+
+/*function Universe() {
+    let instance;
+
+    Universe = function () {
+        return instance;
+    };
+
+    Universe.prototype = this;
+    instance = new Universe();
+    instance.constructor = Universe;
+
+    instance.size = 100;
+    this.getSize = function () {
+        return instance.size
+    };
+    instance.setSize = function (size) {
+        instance.size = size;
+    };
+
+    return instance;
+}*/
+
+
+
+/*function Universe() {
+    let instance;
+    Universe = function Universe() {
+        if (instance) return instance;
+    };
+
+    instance = this;
+
+    instance.size = 100;
+    instance.getSize = function () {
+        return instance.size
+    };
+    instance.setSize = function (size) {
+        instance.size = size;
+    };
+
+    return instance;
+}*/
+
+let Universe = (function() {
+    let instance;
+    
+    function singleton() {
+        if (instance) {
+            return instance
+        } else {
+            instance = this
+        }
+        
+    }
+
+    singleton.size = 100;
+
+    singleton.prototype.getSize = function () {
+        return singleton.size
+    };
+
+    singleton.prototype.setSize = function (size) {
+        singleton.size = size;
+    };
+
+    return singleton
+
+})();
 
 
 let a = new Universe();
