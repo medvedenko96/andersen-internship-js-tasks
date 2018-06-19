1cc9db187ff77cd4b138b971728ce2ac51a3ae4f
Medvedenko Yaroslav <medvedenko96@gmail.com>
2018-06-19 16:18:50 +0300

    fix space

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 function Person(name) {
-	let that = {};
-	that.name = name;
-	that.getName = function () {
-		return that.name;
-	};
-	return that;
+  let that = {};
+  that.name = name;
+  that.getName = function () {
+    return that.name;
+  };
+  return that;
 }
 
 function Man(name, facialHair) {
-	let that = Person(name);
-	that.facialHair = facialHair;
+  let that = Person(name);
+  that.facialHair = facialHair;
 
-	that.getName = function () {
-		return `Name: ${that.name}`;
-	};
+  that.getName = function () {
+    return `Name: ${that.name}`;
+  };
 
-	that.getFacialHair= function (){
-		return that.facialHair;
+  that.getFacialHair= function (){
+    return that.facialHair;
 	};
-	return that;
+	
+  return that;
 }
 
 const person = Person('somebody');
