65a4573646a3eaec31b740e78a57dd4c510f5aca
Medvedenko Yaroslav <medvedenko96@gmail.com>
2018-06-19 16:05:51 +0300

    fix space

--- main.js
+++ main.js
@@ -3,23 +3,23 @@ function Person(name) {
 }
 
 Person.prototype.getName = function() {
-	return this.name;
+  return this.name;
 };
 
 function Man(name, facialHair) {
-	Person.call(this, name);
-	this.facialHair = facialHair;
+  Person.call(this, name);
+  this.facialHair = facialHair;
 }
 
 Man.prototype = Object.create(Person.prototype);
 Man.prototype.constructor = Man;
 
 Man.prototype.getName = function () {
-	return `Name: ${Person.prototype.getName.call(this, this.name)}`;
+  return `Name: ${Person.prototype.getName.call(this, this.name)}`;
 };
 
 Man.prototype.getFacialHair = function () {
-	return this.facialHair;
+  return this.facialHair;
 };
 
 const person = new Person('somebody');
