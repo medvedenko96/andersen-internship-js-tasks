e8a77440b0a7bec3f5273dba2e8bc08d56e84a08
Medvedenko Yaroslav <medvedenko96@gmail.com>
2018-06-19 13:00:47 +0300

    task №3 functional inheritance is done

---
+++ main.js
@@ -0,0 +1,30 @@
+function Person(name) {
+	let that = {};
+	that.name = name;
+	that.getName = function () {
+		return this.name;
+	};
+	return that;
+}
+
+function Man(name, facialHair) {
+	let that = Person(name);
+	that.facialHair = facialHair;
+
+	that.getName = function () {
+		return `Name: ${that.name}`;
+	};
+
+	that.getFacialHair= function (){
+		return that.facialHair;
+	};
+	return that;
+}
+
+const person = Person('somebody');
+console.log(person.getName()); // somebody
+
+const man =  Man('Viktor', true);
+console.log(man);
+console.log(man.getName()); // Name: Viktor
+console.log(man.getFacialHair()); // true
