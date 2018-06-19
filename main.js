50cc8fcff29bfa86bbb5af9c7ca69564dd3d3e1e
Medvedenko Yaroslav <medvedenko96@gmail.com>
2018-06-19 10:37:28 +0300

    task №2 es6 to es5 inheritance is done

---
+++ main.js
@@ -0,0 +1,30 @@
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.getName = function() {
+	return this.name;
+};
+
+function Man(name, facialHair) {
+	Person.apply(this, arguments);
+	this.facialHair = facialHair;
+}
+
+Person.prototype = Object.create(Man.prototype);
+Man.prototype.constructor = Man;
+
+Man.prototype.getName = function () {
+	return `Name: ${Person.prototype.getName.apply(this, arguments)}`;
+};
+
+Man.prototype.getFacialHair = function () {
+	return this.facialHair;
+};
+
+const person = new Person('somebody');
+console.log(person.getName()); // somebody
+
+const man = new Man('Viktor', true);
+console.log(man.getName()); // Name: Viktor
+console.log(man.getFacialHair()); // true
---
+++ mainES6.js
@@ -0,0 +1,31 @@
+class Person {
+	constructor (name) {
+		this.name = name;
+	}
+
+	getName () {
+		return this.name;
+	}
+}
+
+class Man extends Person {
+	constructor (name, facialHair) {
+		super(name);
+		this.facialHair = facialHair;
+	}
+
+	getName () {
+		return `Name: ${super.getName()}`;
+	}
+
+	getFacialHair () {
+		return this.facialHair;
+	}
+}
+
+const person = new Person('somebody');
+console.log(person.getName()); // somebody
+
+const man = new Man('Viktor', true);
+console.log(man.getName()); // Name: Viktor
+console.log(man.getFacialHair()); // true
