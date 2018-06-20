525d5b578a0948a434669951155a98a3e35c2301
Medvedenko Yaroslav <medvedenko96@gmail.com>
2018-06-20 14:09:14 +0300

    fix main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function Person(name) {
-  let that = {};
+  const that = {};
   that.name = name;
 
-  that.getName = function () {
+  that.getName = function getName() {
     return that.name;
   };
 
@@ -10,24 +10,24 @@ function Person(name) {
 }
 
 function Man(name, facialHair) {
-  let that = Person(name);
+  const that = Person(name);
   that.facialHair = facialHair;
 
-  that.getName = function () {
+  that.getName = function getName() {
     return `Name: ${that.name}`;
   };
 
-  that.getFacialHair= function (){
+  that.getFacialHair = function getFacialHair() {
     return that.facialHair;
   };
-  
+
   return that;
 }
 
 const person = Person('somebody');
 console.log(person.getName()); // somebody
 
-const man =  Man('Viktor', true);
+const man = Man('Viktor', true);
 console.log(man);
 console.log(man.getName()); // Name: Viktor
 console.log(man.getFacialHair()); // true
